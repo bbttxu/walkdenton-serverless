95f24f11d4de71ed54b1e7b550fd1f5950c9a77d
Adam Krawiec <adamkrawiec@gmail.com>
2018-12-25 19:27:51 -0600

    sort by distance, and show only 10 results

--- functions/intersections.js
+++ functions/intersections.js
@@ -5,13 +5,19 @@
  * @returns {string}
  */
 
+const { map, take, sortBy, prop } = require("ramda");
+
 const inter = require("../data/inter_lite.json");
 
 const filterLongitudeLatitude = require("../lib/filterLongitudeLatitude");
 
+const distanceFrom = require("../lib/distanceFrom");
+
 module.exports = (longitude = 0, latitude = 0, context, callback) => {
   const start = Date.now();
-  const DELTA = 0.003;
+  const DELTA = 0.002;
+
+  const distanceFromCenter = distanceFrom({ latitude, longitude });
 
   const minLatitude = parseFloat(latitude) - DELTA;
   const maxLatitude = parseFloat(latitude) + DELTA;
@@ -24,9 +30,16 @@ module.exports = (longitude = 0, latitude = 0, context, callback) => {
     latitude: { min: minLatitude, max: maxLatitude }
   })(inter);
 
+  const bar = map(intersection => {
+    intersection.distance = distanceFromCenter(intersection);
+    return intersection;
+  }, foo);
+
+  const baz = take(10, sortBy(prop("distance"), bar));
+
   callback(null, {
-    data: foo,
-    n: foo.length,
+    data: baz,
+    n: baz.length,
     timing: {
       ms: Date.now() - start,
       start
